1b882dce1529f1e03b353da84e34b034a3574109
Stephen Altamirano <stephen@evilrobotstuff.com>
2012-02-23 16:08:12 -0800

    Adds setup script and a slightly nicer package interface

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import distutils.core
+
+distutils.core.setup(
+    name='pldiff',
+    version='1.0',
+    description='Structured Data Diffing',
+    author='Stephen Altamirano',
+    author_email='stephen@theplayforge.com',
+    packages=['libpldiff']
+)
